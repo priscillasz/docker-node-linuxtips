845c1e5e8543f0e7f1832fd51e5d6df32a1add87
Priscilla Souza <81050524+priscillasz@users.noreply.github.com>
2023-07-05 19:07:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Punk do DevOps!');
+  res.send('Alooooooooooooo! Deu bom!');
 });
 
 app.get('*', (req, res) => {
-  res.send('errrrouuuuu!', 404);
+  res.send('Deu ruim!', 404);
 });
 
 app.listen(PORT, HOST);
